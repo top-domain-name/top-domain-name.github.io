7741832db7879a1d2e04fc06f7ae57fe72d5f3ef
liufeifei <759665259@qq.com>
2019-11-12 12:37:44 +0800

    添加第一阶段计算机-大一课程

---
+++ config/config-2-3.js
@@ -0,0 +1,60 @@
+window.config_2_3=[{
+  rank:'start',
+  title:'开始',
+  link:'',
+  noteClass:'config_2_3_empty',
+},{
+  rank:'1-1',
+  title:'计算机专业导论之思维与系统',
+  link:'https://mooc.study.163.com/learn/1000003008?tid=2001381036#/learn/content?type=detail&id=2001684675&cid=2001674809',
+},{
+  rank:'1-2',
+  title:'计算机专业导论之语言与算法',
+  link:'https://mooc.study.163.com/learn/1000004007?tid=2001382039#/learn/content?type=detail&id=2001685452&cid=2001673625',
+},{
+  rank:'1-3',
+  title:'计算机专业导论之学科与专业',
+  link:'https://mooc.study.163.com/learn/1000004008?tid=2001382040#/learn/content?type=detail&id=2001684702&cid=2001674853',
+},{
+  rank:'2-1',
+  title:'计算机专业导论',
+  link:'https://www.icourse163.org/learn/HIT-437006?tid=1206805212#/learn/content',
+},{
+  rank:'2-2',
+  title:'程序设计入门—Python',
+  link:'https://mooc.study.163.com/learn/1000002017?tid=2001354013#/learn/content?type=detail&id=2001640337&cid=2001633766',
+},{
+  rank:'3-1',
+  title:'微积分（一）',
+  link:'https://www.icourse163.org/learn/HIT-153001?tid=1206742217#/learn/content',
+},{
+  rank:'3-2',
+  title:'微积分（二）',
+  link:'https://www.icourse163.org/learn/HIT-207002?tid=1206746217#/learn/content',
+},{
+  rank:'4-1',
+  title:'程序设计入门-C语言',
+  link:'https://mooc.study.163.com/learn/1000002011?tid=2402971000#/learn/content',
+},{
+  rank:'4-2',
+  title:'C语言程序设计进阶',
+  link:'https://mooc.study.163.com/learn/1000004000?tid=2402968001#/learn/content',
+},{
+  rank:'5-1',
+  title:'程序设计入门-Java语言',
+  link:'https://mooc.study.163.com/learn/1000002014?tid=2001462000#/learn/content?type=detail&id=2001777022&cid=2001782076',
+},{
+  rank:'5-2',
+  title:'Java语言程序设计进阶',
+  link:'https://mooc.study.163.com/learn/1000004001?tid=2001463000#/learn/content?type=detail&id=2001778187&cid=2001783256',
+},{
+  rank:'5-3',
+  title:'Java应用技术',
+  link:'',
+  noteClass:'config_2_3_empty',
+},{
+  rank:'end',
+  title:'第一阶段OVER',
+  link:'',
+  noteClass:'config_2_3_empty',
+}];
